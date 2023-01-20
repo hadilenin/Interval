8513de0b523234efdebbef9aadc73ea3f6886c12
hadilenin <hadiyusefi98@gmail.com>
2023-01-20 19:39:29 +0330

    added clamp() method to bound a Number type into an interval, Interval class is now generic

--- Interval.java
+++ Interval.java
@@ -1,54 +1,57 @@
-import java.util.BitSet;
-import java.util.Hashtable;
 import java.util.Random;
 
 import static java.lang.Math.*;
 
-public class Interval implements Comparable,Cloneable {
-    private int leftBound;
-    private int rightBound;
+public class Interval<T extends Number> implements Comparable,Cloneable {
+    private T startBound;
+    private T rightBound;
 
-    public Interval(int var1, int var2) {
-        if (var1 <= var2) {
-            this.leftBound = var1;
+    public Interval(T var1, T var2) {
+        if (getValue(var1) <= getValue(var2)) {
+            this.startBound = var1;
             this.rightBound = var2;
         } else {
-            this.leftBound = var2;
+            this.startBound = var2;
             this.rightBound = var1;
         }
     }
 
     public boolean isOverLapping(Interval interval) {
-        return  isBetween(this.leftBound, interval.leftBound, interval.rightBound) ||
-                isBetween(this.rightBound, interval.leftBound, interval.rightBound) ||
-                isBetween(interval.leftBound, this.leftBound, this.rightBound);
+        return  isBetween(this.startBound, (T) interval.startBound, (T) interval.rightBound) ||
+                isBetween(this.rightBound, (T) interval.startBound, (T) interval.rightBound) ||
+                isBetween((T) interval.startBound, this.startBound, this.rightBound);
     }
 
     public Interval merge(Interval obj) {
         if (isOverLapping(obj)) {
-            int leftBound = min(this.leftBound, obj.leftBound);
-            int rightBound = max(this.rightBound, obj.rightBound);
+            Double leftBound = min(getValue(this.startBound), getValue(obj.startBound));
+            Double rightBound = max(getValue(this.rightBound), getValue(obj.rightBound));
             return new Interval(leftBound, rightBound);
         } else {
             throw new IllegalStateException("Intervals aren't overlapping!!");
         }
     }
 
-    public int clamp(int i){
-        if (i <= leftBound)
-            return leftBound;
-        if (i >= rightBound)
+    public T clamp(T i){
+        if (getValue(i) <= getValue(startBound))
+            return startBound;
+        if (getValue(i) >= getValue(rightBound))
             return rightBound;
         return i;
     }
 
-    private boolean isBetween(int i, int var1, int var2) {
-        return (var1 <= i && i <= var2) || (var2 <= i && i <= var1);
+    private static Double getValue(Number var){
+        return var.doubleValue();
+    }
+
+    private boolean isBetween(T i, T var1, T var2) {
+        return (getValue(var1) <= getValue(i) && getValue(i) <= getValue(var2)) ||
+                (getValue(var2) <= getValue(i) && getValue(i) <= getValue(var1));
     }
 
     @Override
     public String toString() {
-        return "[" + this.leftBound + "," + this.rightBound + "]";
+        return "[" + getValue(startBound) + "," + getValue(rightBound) + "]";
     }
 
     @Override
@@ -59,23 +62,39 @@ public class Interval implements Comparable,Cloneable {
             if (equals(interval))
                 return 0;
 
-            if (this.leftBound < interval.leftBound)
-                return -1 * abs(new Random().nextInt() + 1);
+            if (getValue(this.startBound) < getValue(interval.startBound))
+                return -1;
             else
-                return abs(new Random().nextInt() + 1);
+                return 1;
 
         } else
-            throw new NullPointerException();
+            throw new IllegalArgumentException("Null is not comparable!");
     }
 
     @Override
     public boolean equals(Object o) {
         Interval interval = (Interval) o;
-        return (this.leftBound == interval.leftBound) && (this.rightBound == interval.rightBound);
+        return (this.startBound == interval.startBound) && (this.rightBound == interval.rightBound);
     }
 
     @Override
     protected Object clone() throws CloneNotSupportedException {
-        return new Interval(this.leftBound,this.rightBound);
+        return new Interval(this.startBound,this.rightBound);
+    }
+
+    public T getStartBound() {
+        return startBound;
+    }
+
+    public void setStartBound(T startBound) {
+        this.startBound = startBound;
+    }
+
+    public T getRightBound() {
+        return rightBound;
+    }
+
+    public void setRightBound(T rightBound) {
+        this.rightBound = rightBound;
     }
 }
